b369fa324f98e4c50f48a231717a8b0ecc2d68fb
igor Silva <82171880+igorrzinho@users.noreply.github.com>
2025-10-02 09:46:47 -0300

    Add files via upload

---
+++ SalesWebMvc/Models/ViewsModels/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SalesWebMvc.Models.ViewsModels;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
